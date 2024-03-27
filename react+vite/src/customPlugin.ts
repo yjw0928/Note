5ff26a919a74372f580d8f2766b8a23f01f552c1
yjw <943421434@qq.com>
2024-03-27 22:54:08 +0800

    fix:修改vite

--- react+vite/src/customPlugin.ts
+++ react+vite/src/customPlugin.ts
@@ -2,9 +2,10 @@ import { PluginOption } from 'vite';
 
 const a = (): PluginOption => {
     return {
-        name: 'a',
+        name: 'my-plugin',
+        enforce: 'pre',
         transformIndexHtml: () => {
-            console.log('1111', '22222');
+            console.log('aaaa');
         },
     };
 };
